46a7b55da1267a98330db4567736326046841762
mhay10 <maxwellhay1@gmail.com>
2026-06-05 16:33:23 -0600

    Merge book information from OpenLibrary and Audible

--- src/cli/add_metadata.cpp
+++ src/cli/add_metadata.cpp
@@ -15,41 +15,40 @@ void AddMetadata::run() {
      *
      * - Stop execution if no data found from either source
      *
-     * - Merge collected data into format (OpenLibrary preference):
+     * - Merge collected data into format (Audible preference over OpenLibrary for everything except description):
      *     {title, authors, narrators, description, publish year}
      * - Write metadata to file (ffmpeg)
      */
 
+    // Get OpenLibrary book info
     BookInfo opl_info;
     std::string work = OPL::try_find_book(title, authors);
     if (!work.empty()) {
         fmt::println("Found match in OpenLibrary: {}", work);
         opl_info = OPL::get_book_info(work);
-
-        fmt::println("==== OPENLIBRARY INFO ====");
-        fmt::println("Title: {}", opl_info.title);
-        fmt::println("Authors: {}", opl_info.authors);
-        fmt::println("Narrators: {}", opl_info.narrators);
-        fmt::println("Description: {}", opl_info.description);
-        fmt::println("Year: {}\n", opl_info.year);
     }
 
+    // Get Audible book info
     BookInfo audible_info;
     std::string asin = Audible::try_find_book(title, authors, narrators);
     if (!asin.empty()) {
         fmt::println("Found match in Audible: {}", asin);
         audible_info = Audible::get_book_info(asin);
-
-        fmt::println("==== AUDIBLE INFO ====");
-        fmt::println("Title: {}", audible_info.title);
-        fmt::println("Authors: {}", audible_info.authors);
-        fmt::println("Narrators: {}", audible_info.narrators);
-        fmt::println("Description: {}", audible_info.description);
-        fmt::println("Year: {}\n", audible_info.year);
     }
 
-    if (asin.empty()) {
+    // Stop if nothing was found
+    if (work.empty() && asin.empty()) {
         fmt::println("No match found in OpenLibrary or Audible for: '{}' by '{}'", title, fmt::join(authors, ", "));
         exit(1);
     }
+
+    // Combine info from both sources
+    BookInfo book_info;
+    book_info.title = !audible_info.title.empty() ? audible_info.title : opl_info.title;
+    book_info.authors = !audible_info.authors.empty() ? audible_info.authors : opl_info.authors;
+    book_info.narrators = !audible_info.narrators.empty() ? audible_info.narrators : opl_info.narrators;
+    book_info.description = !opl_info.description.empty() ? opl_info.description : audible_info.description;
+    book_info.year = !audible_info.year.empty() ? audible_info.year : opl_info.year;
+
+    // TODO: Write metadata to file (ffmpeg)
 }
